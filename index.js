6709f5340e58f369422863a571fe30d47dd154f9
migasrcf <miguellgomes64@gmail.com>
2022-05-06 10:39:35 -0300

    criando projeto bare expo

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-const api = require('express')
-const dev =  "desenvolvimento"
+import { registerRootComponent } from 'expo';
+
+import App from './App';
+
+// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
+// It also ensures that whether you load the app in Expo Go or in a native build,
+// the environment is set up appropriately
+registerRootComponent(App);
