503d8e90b488bdd2f154cfc161b055cf8dd7a008
migasrcf <miguellgomes64@gmail.com>
2022-05-09 15:05:09 -0300

    terminada estilizacao da tela Home e criado componentes para usar no estilo da mesma

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,11 +1,12 @@
 import * as React from 'react';
-import { StyleSheet, View, Text, FlatList } from 'react-native';
+import { StyleSheet, View, Text, FlatList, TouchableOpacity, ImageBackground } from 'react-native';
 import {
   useFonts,
   Montserrat_600SemiBold,
   Montserrat_400Regular,
 } from '@expo-google-fonts/montserrat';
 import AppLoading from 'expo-app-loading';
+import modulosData from '../assets/data/modulosData';
 
 const HomeScreen = ({ navigation }) => {
   const [fontsLoaded] = useFonts({
@@ -18,10 +19,27 @@ const HomeScreen = ({ navigation }) => {
 
   const [number, onChangeNumber] = React.useState(null);
 
+  const renderModulosItem = ({ item }) => {
+    return (
+      <TouchableOpacity style={styles.itemModulo}>
+        <ImageBackground source={item.image} style={styles.itemImage}></ImageBackground>
+      </TouchableOpacity>
+    );
+  };
+
   return (
     <View style={styles.body}>
       <View style={styles.bemvindo}>
         <Text style={styles.header1}>Olá, Fulano</Text>
+      </View>
+      <View style={styles.modulosDataWrapper}>
+        <FlatList
+          data={modulosData}
+          renderItem={renderModulosItem}
+          keyExtractor={(item) => item.id}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+        />
       </View>
     </View>
   );
@@ -41,6 +59,17 @@ const styles = StyleSheet.create({
     fontFamily: 'Montserrat_600SemiBold',
     color: '#0A0D36',
     fontSize: 32,
+  },
+  modulosDataWrapper: {
+    flex: 1,
+  },
+  itemModulo: {
+    marginTop: 32,
+    marginLeft: 20,
+  },
+  itemImage: {
+    width: 234,
+    height: 354,
   },
 });
 
