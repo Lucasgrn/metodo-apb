33a6ab1edae3104f82af755f65199cfb8af63736
Lucasgrn <dev01@aeoluscloud.com>
2022-05-10 09:38:11 -0300

    Configuração Inicial do Firebase

---
+++ config/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCx0K3sGxn0M9PseOM3QAuwDK84Ro1C61c",
+    authDomain: "metodo-apb.firebaseapp.com",
+    projectId: "metodo-apb",
+    storageBucket: "metodo-apb.appspot.com",
+    messagingSenderId: "578008982170",
+    appId: "1:578008982170:web:784bfafbe9f5ec26187523",
+    measurementId: "G-VG2DB1WPY4"
+};
+
+const app = initializeApp(firebaseConfig);
