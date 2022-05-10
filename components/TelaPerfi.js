dec1a5ab2e45741a8edc99b270193a61e0205b9c
migasrcf <miguellgomes64@gmail.com>
2022-05-10 10:23:06 -0300

    estruturando Tela de perfil

--- components/TelaPerfi.js
+++ components/TelaPerfi.js
@@ -1,11 +1,15 @@
 import * as React from 'react';
-import { StyleSheet, View, Text, TouchableOpacity, TextInput } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 import {
   useFonts,
   Montserrat_600SemiBold,
   Montserrat_400Regular,
 } from '@expo-google-fonts/montserrat';
 import AppLoading from 'expo-app-loading';
+import perfilData from '../assets/data/perfilData';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+
+MaterialIcons.loadFont();
 
 const TelaPerfil = ({ navigation }) => {
   const [fontsLoaded] = useFonts({
@@ -20,8 +24,29 @@ const TelaPerfil = ({ navigation }) => {
 
   return (
     <View style={styles.body}>
-      <View style={styles.bemvindo}>
-        <Text style={styles.header1}>PERFIL</Text>
+      <View style={styles.perfilWrapper}>
+        <View style={styles.bannerWrapper}></View>
+        <Text style={styles.header1}>Fulano de tal</Text>
+      </View>
+      <View style={styles.treinosWrapper}>
+        <Text>Ultimo treino</Text>
+        <View style={styles.ultimoTreinoWrapper}>
+          <View>
+            <MaterialIcons name="play-circle-fill" size={35} color={'#0A0D36'} />
+          </View>
+          <View>
+            <Text>Treino de ginga</Text>
+            <Text>Nessa aula você vai aprender o movimento de ginga</Text>
+          </View>
+        </View>
+        <Text>Treino Finalizado</Text>
+        <View style={styles.finalizadoTreinoWrapper}>
+          <MaterialIcons name="play-circle-fill" size={35} color={'#0A0D36'} />
+          <View>
+            <Text>Treino de Benção</Text>
+            <Text>Nessa aula você vai aprender o movimento de ginga</Text>
+          </View>
+        </View>
       </View>
     </View>
   );
@@ -32,15 +57,47 @@ const styles = StyleSheet.create({
     backgroundColor: '#FAFCFE',
     flex: 1,
   },
-  bemvindo: {
+  perfilWrapper: {
     alignItems: 'center',
-    marginTop: 120,
+  },
+  bannerWrapper: {
+    alignItems: 'center',
+    backgroundColor: '#C4C4C4',
+    borderBottomLeftRadius: 50,
+    borderBottomRightRadius: 50,
+    height: 155,
+    width: '100%',
   },
   header1: {
     fontFamily: 'Montserrat_600SemiBold',
     color: '#262626',
     fontSize: 24,
-    marginTop: 50,
+    marginTop: 60,
+  },
+  treinosWrapper: {
+    alignItems: 'center',
+    height: 120,
+    marginTop: 45,
+  },
+  ultimoTreinoWrapper: {
+    backgroundColor: '#3cff42',
+    borderRadius: 25,
+    flexDirection: 'row',
+    height: 100,
+    width: '80%',
+    padding: 20,
+    marginTop: 20,
+    marginBottom: 45,
+  },
+  finalizadoTreinoWrapper: {
+    backgroundColor: '#FFFFFF',
+    borderRadius: 25,
+    flexDirection: 'row',
+    height: 100,
+
+    width: '80%',
+    padding: 20,
+    marginTop: 20,
   },
 });
 
