6b5868eda133712ac2c4a0f574912d0fabe2a10e
miguel-oliveiraa <miguellgomes64@gmail.com>
2022-05-18 10:48:16 -0300

    terminada tela de video e feita algumas mudancas na estrutura dos arquivos

--- components/Capoeira.js
+++ components/Capoeira.js
@@ -1,33 +1,28 @@
-import * as React from 'react';
-import {
-  StyleSheet,
-  View,
-  ImageBackground,
-  TouchableOpacity,
-  Text,
-  ScrollView,
-  SafeAreaView,
-} from 'react-native';
+import * as React from "react";
+import { StyleSheet, View, Text } from "react-native";
 import {
   useFonts,
   Montserrat_600SemiBold,
   Montserrat_400Regular,
-} from '@expo-google-fonts/montserrat';
-import AppLoading from 'expo-app-loading';
-import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
-import { Dimensions } from 'react-native';
+} from "@expo-google-fonts/montserrat";
+import AppLoading from "expo-app-loading";
+import MaterialIcons from "react-native-vector-icons/MaterialIcons";
+import { Dimensions } from "react-native";
 import YoutubePlayer from "react-native-youtube-iframe";
+import { StatusBar } from "react-native";
 
-const windowWidth = Dimensions.get('window').width;
-const videoWidth = windowWidth * 0.8;
+const statusBarHeight =
+  Platform.OS === "ios" ? 0 : ("statusBarHeight: ", StatusBar.currentHeight);
+
+const windowWidth = Dimensions.get("window").width;
+const videoWidth = windowWidth;
 const videoHeight = videoWidth * 0.5625;
 const imageHeight = windowWidth * 1.07;
-const windowHeight = Dimensions.get('window').height;
+const windowHeight = Dimensions.get("window").height;
 
 MaterialIcons.loadFont();
 
 const Capoeira = ({ navigation }) => {
-
   const [fontsLoaded] = useFonts({
     Montserrat_600SemiBold,
     Montserrat_400Regular,
@@ -39,23 +34,31 @@ const Capoeira = ({ navigation }) => {
   return (
     <View style={styles.body}>
       <View>
-      <YoutubePlayer
-      width={videoWidth}
-        height={videoHeight}
-        play={true}
-        videoId={"erInHkjxkL8"}
-      />
+        <View style={styles.video__youtubeplayer}>
+          <YoutubePlayer
+            width={videoWidth}
+            height={videoHeight}
+            play={true}
+            videoId={"erInHkjxkL8"}
+          />
+        </View>
       </View>
-      <Text>Mds</Text>
     </View>
-  )}
+  );
+};
 
 const styles = StyleSheet.create({
-    body: {
-      backgroundColor: '#FAFCFE',
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
-    },})
+  body: {
+    backgroundColor: "#FAFCFE",
+    flex: 1,
+    alignItems: "center",
+    marginTop: statusBarHeight,
+  },
+  video__iconeVoltar: {
+    marginLeft: 25,
+    marginTop: 20,
+    marginBottom: -55,
+  },
+});
 
-    export {Capoeira}
+export { Capoeira };
---
+++ components/index.js
@@ -0,0 +1,56 @@
+// In App.js in a new project
+
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { TelaLogin } from "./TelaLogin";
+import { NavegadorApp } from "./NavegadorApp";
+import {
+  useFonts,
+  Montserrat_600SemiBold,
+  Montserrat_400Regular,
+} from "@expo-google-fonts/montserrat";
+import AppLoading from "expo-app-loading";
+import { TelaTreino } from "./telaTreino";
+import { Capoeira } from "./Capoeira";
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  const [fontsLoaded] = useFonts({
+    Montserrat_600SemiBold,
+    Montserrat_400Regular,
+  });
+  if (!fontsLoaded) {
+    <AppLoading />;
+  }
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="TelaLogin"
+          component={TelaLogin}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="NavegadorApp"
+          component={NavegadorApp}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="TelaTreino"
+          component={TelaTreino}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Capoeira"
+          component={Capoeira}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
